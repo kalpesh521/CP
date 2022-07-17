df84c154572455c5d09402f1720f6cb742c44d75
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-07-17 19:52:36 +0530

    Pairs

---
+++ C++ Practice Set/30Pairs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    pair<int, string> p;
+    // p=make_pair(2,"KPP");// Method 1
+    p = {1, "kp"}; // Method 2
+    cout << p.first << " " << p.second<<endl;
+    pair<int, string> &p1 = p;
+    p1.first = 6;
+    cout << p.first << " " << p.second<<endl;
+    return 0;
+}
