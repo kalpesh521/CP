e86bf631541edd963a3ac429aeb5b08e70f3b35f
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-07-18 11:33:10 +0530

    Iterators

--- C++ Practice Set/31Vector.cpp
+++ C++ Practice Set/31Vector.cpp
@@ -14,9 +14,9 @@ void display(vector<T> &v)
 }
 int main(int argc, char const *argv[])
 {
-    //Vector 1
-    vector<int> vec1; // zero element length vector
-    int element, size=5 ;
+    // Vector 1
+   /* vector<int> vec1; // zero element length vector
+    int element, size = 5;
     cout << "Enter size of vector" << endl;
     cin >> size;
 
@@ -26,33 +26,38 @@ int main(int argc, char const *argv[])
              << " ";
         cin >> element;
         vec1.push_back(element); // Add element at end
+    }*/
+
+    /*  vec1.pop_back(); // Delete element at last
+      display(vec1);
+
+      vector<int>::iterator iter = vec1.begin();//Replace by auto keyword
+      vec1.insert(iter + 1, 5, 566); // 5-> 5 copies
+  */
+
+    /*
+        vector<char> vec2(4); // 4 element  length vector
+        vec2.push_back('8');
+        // vec2.push_back('8');
+        //  display(vec2);
+
+        vector<char> vec3(vec2); // 4th element vector derived from vec2
+        display(vec3);
+
+        vector<int> vec4(6, 2); // Display 6 elements of 2;
+        display(vec4);
+    */
+
+    vector<pair<int, int>> vec = {{3, 4}, {2, 6}, {1, 8}};
+    vector<pair<int, int>> :: iterator it;
+    for (int it = vec.begin(); it != vec.end(); ++it)
+    {
+        cout << (*it).first << (*it).second << endl;
+    }
+    for (int it = vec.begin(); it != vec.end(); ++it)
+    {
+        cout << (it->first) << (it->second )<< endl;
     }
 
-  /*  vec1.pop_back(); // Delete element at last
-    display(vec1);  
-
-    vector<int>::iterator iter = vec1.begin();
-    vec1.insert(iter + 1, 5, 566); // 5-> 5 copies
-*/
-
-
-
-
-
-
-
-
-/*
-    vector<char> vec2(4); // 4 element  length vector
-    vec2.push_back('8');
-    // vec2.push_back('8');
-    //  display(vec2);
-
-    vector<char> vec3(vec2); // 4th element vector derived from vec2
-    display(vec3);
-
-    vector<int> vec4(6, 2); // Display 6 elements of 2;
-    display(vec4);
-*/
     return 0;
 }
