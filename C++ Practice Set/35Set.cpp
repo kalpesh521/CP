a232f4a8694b3e13692426a6912671f8511f9436
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-07-19 09:57:14 +0530

    Multiset

--- C++ Practice Set/35Set.cpp
+++ C++ Practice Set/35Set.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include <set>
 using namespace std;
-void print_set(unordered_set<string> &s)
+void print_set(multiset<string> &s)
 {
     for (string value : s)
     {
@@ -9,8 +9,9 @@ void print_set(unordered_set<string> &s)
     }
 }
 int main(int argc, char const *argv[])
-{ // Ordered Output
+{   // Ordered Output
     // Store Unique Elements
+    // uses only keys
     /*set<string> s;
     s.insert("abv"); // log(n)
     s.insert("tre");
@@ -40,37 +41,53 @@ int main(int argc, char const *argv[])
      print_set(s);
  */
     // Unordered Set
+    // Like hash tables/Functions
+    /* unordered_set<string> s;
+     int n;
+     cout << "Enter Size" << endl;
+     cin >> n;
+     for (int i = 0; i < n; ++i)
+     {
+         string str;
+         cout << "Enter String" << endl; // O(1)
+         cin >> str;
+         s.insert(str);
+     }
+     int q;
+     cout << "Enter Size Of q" << endl;
+     cin >> q;
+     while (q--)
+     {
+         string str;
+         cout << "Enter String" << endl;
+         cin >> str;
+         if (s.find(str) == s.end())
+         {
+             cout << "No" << endl;
+         }
+         else
+         {
+             cout << "Yes" << endl;
+         }
+     }
+     cout << endl;
+     cout << "Display set" << endl;
+     print_set(s);*/
 
-    unordered_set<string> s;
-    int n;
-    cout << "Enter Size" << endl;
-    cin >> n;
-    for (int i = 0; i < n; ++i)
-    {
-        string str;
-        cout << "Enter String" << endl;
-        cin >> str;
-        s.insert(str);
-    }
-    int q;
-    cout<<"Enter Size Of q"<<endl;
-    cin >> q;
-    while (q--)
-    {
-        string str;
-        cout<<"Enter String"<<endl;
-        cin >> str;
-        if (s.find(str) == s.end())
-        {
-            cout << "No" << endl;
-        }
-        else
-        {
-            cout << "Yes" << endl;
-        }
-    }
-    cout << endl;
-    cout << "Display set" << endl;
+    // Multiset
+    // Contain duplicate Value
+    multiset<string> s;
+    s.insert("abc");
+    s.insert("erc");
+    s.insert("zbc");
+    s.insert("abc");
+    // auto it = s.find("abc");
+    // if (it != s.end())
+    // {
+    //     s.erase(it);
+    // }
+    s.erase("abc");
     print_set(s);
+
     return 0;
 }
