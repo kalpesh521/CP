e2b4833c0bd37738c5d18ddd4cdb0afcacdaee27
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-07-19 09:42:28 +0530

    Unordered_Set

--- C++ Practice Set/35Set.cpp
+++ C++ Practice Set/35Set.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include <set>
 using namespace std;
-void print_set(set<string> &s)
+void print_set(unordered_set<string> &s)
 {
     for (string value : s)
     {
@@ -9,19 +9,68 @@ void print_set(set<string> &s)
     }
 }
 int main(int argc, char const *argv[])
-{   // Ordered Output
+{ // Ordered Output
     // Store Unique Elements
-    set<string> s;
+    /*set<string> s;
     s.insert("abv"); // log(n)
     s.insert("tre");
     s.insert("ert");
-    auto it = s.find("abv");
+    auto it = s.find("ert");
     if (it != s.end())
     {
         // cout << *it << endl;
-         s.erase(it) ;
+        s.erase(it);
     }
     print_set(s);
+*/
+    // Ordered Set
+    /* set<string> s;
+     int n;
+     cout << "Enter Size" << endl;
+     cin >> n;
+     for (int i = 0; i < n; ++i)
+     {
+         string str;
+         cout << "Enter String" << endl;
+         cin >> str;
+         s.insert(str);
+     }
+     cout << endl;
+     cout << "Display set" << endl;
+     print_set(s);
+ */
+    // Unordered Set
 
+    unordered_set<string> s;
+    int n;
+    cout << "Enter Size" << endl;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        string str;
+        cout << "Enter String" << endl;
+        cin >> str;
+        s.insert(str);
+    }
+    int q;
+    cout<<"Enter Size Of q"<<endl;
+    cin >> q;
+    while (q--)
+    {
+        string str;
+        cout<<"Enter String"<<endl;
+        cin >> str;
+        if (s.find(str) == s.end())
+        {
+            cout << "No" << endl;
+        }
+        else
+        {
+            cout << "Yes" << endl;
+        }
+    }
+    cout << endl;
+    cout << "Display set" << endl;
+    print_set(s);
     return 0;
 }
