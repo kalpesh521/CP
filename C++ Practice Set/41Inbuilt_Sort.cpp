7894868869d166440b0b48e33f8779bd8eda0914
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-07-22 10:33:29 +0530

    Inbuilt Sort Vectors:

--- C++ Practice Set/41Inbuilt_Sort.cpp
+++ C++ Practice Set/41Inbuilt_Sort.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <vector>
 int main(int argc, char const *argv[])
 {
     int n;
-    cin >> n ;
-    int a[n];
+    cin >> n;
+   // int a[n];
+    vector<int> a(n);
     for (int i = 0; i < n; ++i)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    sort(a, a + n);
-    for (int i = 0; i < n ; ++i)
-    { 
-        cout<<"Sorted array is :"<<endl;
+   // sort(a, a + n);
+    //sort(a+2, a + n);
+    //sort(a, a + 3);
+    sort(a.begin(),a.end());
+    //Intro Sort => Quick Sort ,Heap Sort and Insertion sort 
+    cout << "Sorted array is :" << endl;
+
+    for (int i = 0; i < n; ++i)
+    {
         cout << a[i] << endl;
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
