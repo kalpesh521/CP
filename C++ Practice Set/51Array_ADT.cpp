d770a157137b558dd5677b8b4e0dfe586b4897e9
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-07-26 19:09:59 +0530

    Insertion and deletion

--- C++ Practice Set/51Array_ADT.cpp
+++ C++ Practice Set/51Array_ADT.cpp
@@ -7,13 +7,6 @@ struct Array
     int length;
 };
 
-void Append(struct Array *arr,int x)
-{
-    if (arr->length < arr->size)
-    {
-        arr->A[arr->length++] = x;
-    }
-}
 void Display(struct Array arr)
 {
     for (int i = 0; i < arr.length; i++)
@@ -21,12 +14,49 @@ void Display(struct Array arr)
         cout << arr.A[i] << endl;
     }
 }
+void Append(struct Array *arr, int x)
+{
+    if (arr->length < arr->size)
+    {
+        arr->A[arr->length++] = x;
+    }
+}
+
+void insert(struct Array *arr, int index, int x)
+{
+    for (int i = arr->length; index < i; i--)
+    {
+        arr->A[i] = arr->A[i - 1];
+    }
+    arr->A[index] = x;
+    arr->length++;
+}
+
+int Delete (struct Array *arr, int index)
+{
+    int x = 0;
+    int i;
+    if (index >= 0 && arr->length > index)
+    {
+        x = arr->A[index];
+        for (i = index; i < arr->length - 1; i++)
+        {
+            arr->A[i] = arr->A[i + 1];
+        }
+        arr->length--;
+        return x;
+    }
+    return 0;
+}
+ 
 int main(int argc, char const *argv[])
 {
 
     struct Array arr = {{1, 23, 4, 5, 6, 7}, 10, 6};
-    Append(&arr, 10);
-        Display(arr);
+    // Append(&arr, 10);
+    // insert(&arr, 3, 6);
+    Delete(&arr, 0);
+    Display(arr);
 
     return 0;
 }
