3f9ea4773513c6e39fd9895cc29683c2082cb6e2
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-07-29 17:23:23 +0530

    Reverse_Sort

--- DSA/3_Array_ADT.cpp
+++ DSA/3_Array_ADT.cpp
@@ -137,7 +137,7 @@ void Set(struct Array *arr, int index, int x)
 
 int Max(struct Array arr)
 {
-    int max =arr.A[0] ;
+    int max = arr.A[0];
     int i;
     for (i = 1; i < arr.length; i++)
     {
@@ -177,10 +177,86 @@ float Avg(struct Array arr)
 {
     return (float)Sum(arr) / arr.length;
 }
+
+// Reverse
+// Method 1
+
+void Reverse(struct Array *arr)
+{
+    int *B;
+    int i, j;
+    B = new int[arr->length];
+    // B=(int *)malloc(arr->length*sizeof(int));
+    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
+    {
+        B[j] = arr->A[i];
+    }
+    for (int i = 0; i < arr->length; i++)
+    {
+        arr->A[i] = B[i];
+    }
+}
+
+// Method 2
+
+void Reverse2(struct Array *arr)
+{
+    int i, j;
+    for (i = 0, j = arr->length - 1; i < j; i++, j--)
+    {
+        swap(arr->A[i], arr->A[j]);
+    }
+}
+
+void InsertEle(struct Array *arr, int x)
+{
+    int i = arr->length - 1;
+
+    if (arr->length == arr->size)
+    {
+        return;
+    }
+    if (i >= 0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+int isSorted(struct Array arr)
+{
+    int i;
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+            return 0;
+    }
+    return 1;
+}
+
+void Rearange(struct Array *arr)
+{
+    int i = 0;
+    int j = arr->length - 1;
+    while (i < j)
+    {
+        while (arr->A[i] < 0)
+            i++;
+        while (arr->A[i] >= 0)
+            j--;
+
+        if (i < j)
+        {
+            swap(&arr->A[i], &arr->A[j]);
+        }
+    }
+}
 int main(int argc, char const *argv[])
 {
 
-    struct Array arr = {{1, 23, 4, 5, 6, 7}, 10, 6};
+    //  struct Array arr = {{1, 23, 4, 5, 6, 7}, 10, 6};
+    struct Array arr = {{1, -2, 4, -5, 6, -9}, 10, 6}; // Sorted Array
     // Append(&arr, 10);
     // insert(&arr, 3, 6);
     // Delete(&arr, 0);
@@ -189,11 +265,15 @@ int main(int argc, char const *argv[])
     // cout << RecBinSearch(arr.A, 0, arr.length, 4) << endl;
     // cout << "Display  Array" << endl;
     // cout<<Get(arr, 4)<<endl;
-    //Set(&arr, 4, 25);
-    cout << Max(arr) << endl;
-    cout << Min(arr) << endl;
-    cout<<Sum(arr)<<endl;
-    cout<<Avg(arr)<<endl;
+    // Set(&arr, 4, 25);
+    // cout << Max(arr) << endl;
+    // cout << Min(arr) << endl;
+    // cout << Sum(arr) << endl;
+    // cout << Avg(arr) << endl;
+    // Reverse2(&arr);
+    // InsertEle(&arr, 8);
+    //cout << isSorted(arr) << endl;
+    Rearange(&arr);
     Display(arr);
 
     return 0;
