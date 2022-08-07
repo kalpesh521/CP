bc6a43889dfac3615285945eab479ad7ae325bcf
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-07 22:38:11 +0530

    Recursion

--- C++ Practice Set/52Recursion.cpp
+++ C++ Practice Set/52Recursion.cpp
@@ -1,17 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void fun1(int n){
-    if (n>0){
-   fun1(n-1);
-      cout<<n<<" ";
-
+void fun1(int n)
+{
+    if (n > 0)
+    {
+        fun1(n - 1);
+        cout << n << " ";
     }
+}
+
+int fun2(int n){
+    if (n>0)
+    {
+        return fun2(n-1)+n;
+    }
+    return 0;
 }
 int main(int argc, char const *argv[])
 {
 
-    int x =5;
+    int x = 5;
+    int y = 5;
     fun1(x);
- return 0;
-} 
+    cout<<endl;
+    cout<<fun2(y);
+    return 0;
+}
