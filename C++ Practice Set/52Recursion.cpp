48908eab9e63d20e6072b94ba94b11bc5f7a38cd
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-08 17:35:30 +0530

    Recursion

--- C++ Practice Set/52Recursion.cpp
+++ C++ Practice Set/52Recursion.cpp
@@ -9,11 +9,12 @@ void fun1(int n)
         cout << n << " ";
     }
 }
-
-int fun2(int n){
-    if (n>0)
-    {
-        return fun2(n-1)+n;
+//int x=0; // Global Variable
+int fun2(int n)
+{   static int x=0;
+    if (n > 0)
+    {   x++;
+        return fun2(n - 1) + x;
     }
     return 0;
 }
@@ -23,7 +24,7 @@ int main(int argc, char const *argv[])
     int x = 5;
     int y = 5;
     fun1(x);
-    cout<<endl;
-    cout<<fun2(y);
+    cout << endl;
+    cout << fun2(y);
     return 0;
 }
