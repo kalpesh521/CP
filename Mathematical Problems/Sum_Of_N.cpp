ad444792ce08cb0b47dd54ac5b79c233e1de34d5
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-09 16:54:13 +0530

    Sum Of N

---
+++ Mathematical Problems/Sum_Of_N.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int n)
+{
+    return n * (n + 1) / 2; // O(n)
+}
+
+int rec_sum(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum(n - 1) + n;
+    }
+}
+int main(int argc, char const *argv[])
+{
+
+    cout << sum(10);
+    cout << endl;
+    int sum = 0;
+    int n = 10;
+    for (int i = 1; i <= n; i++) // O(n)
+    {
+        sum = sum + i;
+    }
+    cout << sum;
+
+    cout << endl;
+    cout << rec_sum(10) ;return 0;
+}
