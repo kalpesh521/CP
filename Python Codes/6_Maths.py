0a510a14c7713e8d57014927c40cc1fbc4ab0864
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-19 16:18:02 +0530

    math

---
+++ Python Codes/6_Maths.py
@@ -0,0 +1,15 @@
+#%%
+import math
+from operator import imod
+print(math.sqrt(25))
+#%%
+import math as m 
+print(math.floor(2.7)) #returns smaller integer
+print(math.ceil(2.7))  #returns greatest integer
+
+#%%
+from math import pow,sqrt,cos
+print(sqrt(36))
+print(pow(2,8))
+print(cos(45))
+print(help('math'))
