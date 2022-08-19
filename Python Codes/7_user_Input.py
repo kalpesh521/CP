9704dc75b8d070a3651a286ba7ffd2de90b827ea
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-19 16:52:20 +0530

    User Input

---
+++ Python Codes/7_user_Input.py
@@ -0,0 +1,25 @@
+#%%
+from tkinter import Y
+x=int(input("Enter first number"))
+y=int(input("Enter second number"))
+z =x+y
+print("The  Sum is ")
+print(z)
+
+#%%
+# Command Line
+# import sys
+# x=int(sys.argv[1])
+# y=int(sys.argv[2])
+# z=x+y 
+# print(z)
+# print(x)
+# print(y)
+
+#%%
+a= eval(input("Enter Expression "))
+print(a)
+
+#%%
+b= input("Enter")[2]
+print(b)
