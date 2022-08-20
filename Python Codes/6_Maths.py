bdab2ef5b47b38b66ff00e0a1f62aaf2d5708ac3
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-20 17:16:47 +0530

    Prime Number

--- Python Codes/6_Maths.py
+++ Python Codes/6_Maths.py
@@ -12,4 +12,14 @@ from math import pow,sqrt,cos
 print(sqrt(36))
 print(pow(2,8))
 print(cos(45))
-print(help('math'))
+#print(help('math'))
+
+#%%
+#To check number prime or not
+num =11
+for i in range(2,num):
+    if num%i==0:
+        print("Number is Non-prime")
+        break
+else:
+    print("Number is prime")
