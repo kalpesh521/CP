61fc1c313a9818d387af2bac68bcde4989dd255f
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-22 22:59:06 +0530

    Array

--- Python Codes/10_Array.py
+++ Python Codes/10_Array.py
@@ -1,47 +1,58 @@
-#import array as arr
-from array import *
-val =array('i',[2,5,4,7,1])
-#val.reverse()
-print(val.buffer_info()) #print address and Size of an array
-print(val)
-for i in range(5):
-    print(val[i])
+# #import array as arr
+# from array import *
+# val =array('i',[2,5,4,7,1])
+# #val.reverse()
+# print(val.buffer_info()) #print address and Size of an array
+# print(val)
+# for i in range(5):
+#     print(val[i])
     
-print("Another Method")
-for i in val:
-    print(i)
+# print("Another Method")
+# for i in val:
+#     print(i)
     
-print("Method to create new array")
+# print("Method to create new array")
 
-newarr = array(val.typecode,(a for a in val))
+# newarr = array(val.typecode,(a for a in val))
 
-for i in newarr:
-    print(i)
-print("Square of elements of array")
-newarr2 = array(val.typecode,(a*a for a in val))
+# for i in newarr:
+#     print(i)
+# print("Square of elements of array")
+# newarr2 = array(val.typecode,(a*a for a in val))
 
-for i in newarr2:
-    print(i)
+# for i in newarr2:
+#     print(i)
     
-i =0
-while(i<len(newarr2)):
-    print(newarr2[i])
-    i+=1
-#%%   
-from array import *
-print("User Input")
-n=int(input("Enter length of an array"))
-arr=array('i',[])
-for i in range(n):
-    x=int(input("Enter the value"))
-    arr.append(x)
+# i =0
+# while(i<len(newarr2)):
+#     print(newarr2[i])
+#     i+=1
+# #%%   
+# from array import *
+# print("User Input")
+# n=int(input("Enter length of an array : "))
+# arr=array('i',[])
+# for i in range(n):
+#     x=int(input("Enter the value : "))
+#     arr.append(x)
     
-print(arr)
+# print(arr)
 
-val=int(input("Enter  the value to search"))
+# val=int(input("Enter  the value to search : "))
 
-k=0
-for e in arr:
-    if e==val:
-        print(k)
-    k+=1
+# k=0
+# for e in arr:
+#     if e==val:
+#         print(k)
+#         break
+#     k+=1
+# #%%
+from numpy import *
+arr1=array([2,3,56,7,2])
+arr2=array([22,13,6,17,52])
+
+arr2=arr1.view()
+#arr2=arr1.copy()
+arr1[2]=5
+print(arr1)
+print(arr2)
