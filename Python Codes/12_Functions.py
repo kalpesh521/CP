194dad015c6a7a4367ec63a85cacf7de1266e63f
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-23 18:07:20 +0530

    Function

--- Python Codes/12_Functions.py
+++ Python Codes/12_Functions.py
@@ -43,7 +43,7 @@ key('Ram',age=23,Loc="Pune",Mob=3232324121)
 
 #Global and Local Variable function
 
-a =5
+a =5  
 def glob():
     global a
     a =3
@@ -52,3 +52,38 @@ def glob():
 
 glob()
 print(a)
+
+#%%
+#Count even /odd number in list 
+lst =[3,4,12,67,32,5,6,2,4,11]
+def count(lst):
+    even =0
+    odd =0
+    for i in lst:
+        if i%2==0:
+            even+=1
+        else:
+            odd+=1
+    return even,odd
+even,odd =count(lst)
+print("Even : {} and Odd : {}".format(even,odd))
+
+#%%
+
+n =int(input("Enter number : "))
+def fib(n):
+    
+    
+     a=0
+     b=1
+     if n==1:
+         print(a)
+     else:
+         print(a)
+         print(b)
+     for i in range(2,n):
+         c=a+b
+         a=b
+         b=c
+         print(c)
+fib(n)
