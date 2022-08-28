84cbe51131c4da58cb9cbcba7530dc9ea4f6056c
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-28 16:41:11 +0530

    Inheritence

---
+++ Python Codes/22_Inheritence.py
@@ -0,0 +1,18 @@
+
+class A:
+    def feature1(self):
+        print("Feature 1")
+    def feature2(self):
+        print("Feature 2")
+class B:
+    def feature3(self):
+        print("Feature 3")
+    def feature4(self):
+        print("Feature 4")
+class C:
+    def feature5(self):
+        print("Feature 5")
+a1=A()
+b1=B()
+a1.feature1()
+a1.feature2()
