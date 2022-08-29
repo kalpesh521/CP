4b985fb5cbae6e990273f24d27a6932389755aa2
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-08-29 16:25:11 +0530

    Iterator

---
+++ Python Codes/27_Iterator.py
@@ -0,0 +1,21 @@
+from multiprocessing.sharedctypes import Value
+
+
+class Topten:
+    def __init__(self):
+        self.num=1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.num <=10:
+            val = self.num 
+            self.num +=1
+            return val
+        else:
+            raise StopIteration
+t1=Topten()
+for i in t1:
+    print(i) 
+            
+    
+    
