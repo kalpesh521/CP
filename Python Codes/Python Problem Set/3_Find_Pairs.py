6a0c8525615c3d61163f6ec64e09c5a1eeedc8fb
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-09-01 11:32:26 +0530

    Pairs

---
+++ Python Codes/Python Problem Set/3_Find_Pairs.py
@@ -0,0 +1,10 @@
+
+l=[1,2,3,5,7,11]
+def find_pairs(l,x):
+    pairs=[]
+    for (i,el1) in enumerate(l):
+        for (j,el2) in enumerate(l[i+1:]):
+            if el1+el2 ==x:
+                pairs.append((el1,el2))
+    return pairs 
+print(find_pairs(l,5))     
