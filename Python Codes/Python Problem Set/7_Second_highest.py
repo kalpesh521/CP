61cbd0c63ef378fc7588d59df0dea78415c746d7
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-09-02 19:23:39 +0530

    Pushed

--- Python Codes/Python Problem Set/7_Second_highest.py
+++ Python Codes/Python Problem Set/7_Second_highest.py
@@ -1,13 +1,17 @@
 
-list1=[]
-n=int(input())
-list1=list(map(int,input().split()))
-#list2=int(list1)
-def large(list1):
-    length =len(list1)
-    list2=set(list1)
-    sorted(list2)
-    print(list2[length-2])
-    
- 
-large(list1)
+# list1=[]
+# n=int(input())
+# list1=list(map(int,input().split()))#string to int input 
+# def large(list1):
+#     length =len(list1)
+#     list1.sort()
+#     print(list1[length-2])
+# large(list1)
+n = int(input())
+arr =list(map(int, input().split()))
+
+a=max(arr)
+c=arr.count(a)
+for i in range(c):
+    arr.remove(a)
+print(max(arr))
