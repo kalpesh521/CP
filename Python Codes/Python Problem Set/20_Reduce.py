f7eb8040b009409bef4d70514a73b99166265a86
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-09-16 16:09:05 +0530

    Reduce

--- Python Codes/Python Problem Set/20_Reduce.py
+++ Python Codes/Python Problem Set/20_Reduce.py
@@ -1,6 +1,19 @@
+lst=[2,3,4,5,6]
+from functools import reduce  # Return last summation result
+from itertools import accumulate # Returns iterator containing intermediate result 
+import operator 
 
-from functools import reduce
 print(reduce(lambda a,b:a*b ,lst))
+print(reduce(operator.mul ,lst))
+print(list(accumulate(lst,lambda a,b:a*b)))
+
 print(reduce(lambda a,b:a+b ,lst))
+print(reduce(operator.add,lst))
+print(list(accumulate(lst,lambda a,b:a+b)))
 
 print(reduce(lambda a,b:a if a>b else b,lst))
+
+#Reduce three parameter 
+# reduce(function,iterable,initializer)
+print(reduce(lambda a,b:a+b ,lst,6))
+# initializer first add default value then add interable
