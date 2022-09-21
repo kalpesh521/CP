bf97a1c955d47a2fc41943892a11acc24083df49
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-09-21 19:16:02 +0530

    Input

--- Python Codes/7_user_Input.py
+++ Python Codes/7_user_Input.py
@@ -1,25 +1,42 @@
-#%%
-from tkinter import Y
-x=int(input("Enter first number"))
-y=int(input("Enter second number"))
-z =x+y
-print("The  Sum is ")
-print(z)
 
-#%%
-# Command Line
-# import sys
-# x=int(sys.argv[1])
-# y=int(sys.argv[2])
-# z=x+y 
+# from tkinter import Y
+# x=int(input("Enter first number"))
+# y=int(input("Enter second number"))
+# z =x+y
+# print("The  Sum is ")
 # print(z)
+
+
+# # Command Line
+# # import sys
+# # x=int(sys.argv[1])
+# # y=int(sys.argv[2])
+# # z=x+y 
+# # print(z)
+# # print(x)
+# # print(y)
+
+
+# a= eval(input("Enter Expression "))
+# print(a)
+
+
+# b= input("Enter")[2]
+# print(b)
+
+# x,y,z= input("Enter three Values\n").split()
 # print(x)
 # print(y)
+# print(z)
 
-#%%
-a= eval(input("Enter Expression "))
-print(a)
-
-#%%
-b= input("Enter")[2]
-print(b)
+# x,y,z = [int(x) for x in input('Enter input \n').split()]
+# print(x)
+# print(y)
+# print(z)
+# x = [int(x) for x in input('Enter Multiple input \n').split(",")]
+# print(x)
+ 
+kp="India is my country"
+print(kp,end="@")
+print()
+print(kp.split(),sep='')
