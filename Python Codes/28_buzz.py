8a89db13f57e5cb73c20b1a253642c991803af8a
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-09-22 14:47:42 +0530

    Loop

---
+++ Python Codes/28_buzz.py
@@ -0,0 +1,11 @@
+for i in range(1,100):
+    if i%3==0 and i%5==0:
+        print("Fuzzbuzz")
+    elif i%3==0:
+        print("Fuzz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
+
+        
