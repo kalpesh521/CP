3a99ee2b3628a8009cae24bd13fa2434531092a5
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-10-06 19:24:42 +0530

    factorial Push

--- Java/Java Codes/Java Problem Set/Factorial.java
+++ Java/Java Codes/Java Problem Set/Factorial.java
@@ -1,5 +1,5 @@
 class Factorial {
-        // program to find factorial
+        // find factorial
         public static void main(String args[]) {
         int i, fact = 1;
         int number = 5;// It is the number to calculate factorial
