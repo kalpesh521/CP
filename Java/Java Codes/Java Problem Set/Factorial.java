1019980771412d4ea037eec920bbd472c7733ce0
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-10-12 21:53:34 +0530

    zombie

--- Java/Java Codes/Java Problem Set/Factorial.java
+++ Java/Java Codes/Java Problem Set/Factorial.java
@@ -1,5 +1,5 @@
 class Factorial {
-        //  factorial java program 
+        //  factorial java 
         public static void main(String args[]) {
         int i, fact = 1;
         int number = 5;// It is the number to calculate factorial
