bfa49ab91f8b504b21e60100336fa36bfa862221
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-10-13 10:55:18 +0530

    knapsack

--- DSA/zombie.c
+++ DSA/zombie.c
@@ -1,69 +1,71 @@
- #include<stdio.h>
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int a[30], count;
+int place(int i, int j)
+{
+      int k;
+      for (k = 1; k <= i - 1; k++)
+      {
+            if (a[k] == j)
+            {
+                  return 0;
+            }
+            else if (abs(a[k] - j) == abs(k - i))
+            {
+                  return 0;
+            }
+      }
+      return 1;
+}
+void print_func(int n)
+{
+      int i, j, k;
+      printf("\n solution number=%d\n", ++count);
+      for (i = 1; i <= n; i++)
+      {
+            printf("\t%d", i);
+      }
+      for (i = 1; i <= n; i++)
+      {
+            printf("\n %d", i);
+            for (j = 1; j <= n; j++)
+            {
+                  if (a[i] == j)
+                  {
+                        printf("\t Q");
+                  }
+                  else
+                  {
+                        printf("\t -");
+                  }
+            }
+      }
+}
+void queen(int i, int n)
+{
+      int j, k;
+      for (j = 1; j <= n; j++)
+      {
+            if (place(i, j))
+            {
+                  a[i] = j;
+                  if (i == n)
+                  {
+                        print_func(n);
+                  }
+                  else
+                  {
+                        queen(i + 1, n);
+                  }
+            }
+      }
+}
 int main()
 {
-      int i, limit, total = 0, x, counter = 0, time_quantum;
-      int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10], temp[10];
-      float average_wait_time, average_turnaround_time;
-      printf("\nEnter Total Number of Processes:\t");
-      scanf("%d", &limit);
-      x = limit;
-      for(i = 0; i < limit; i++)
-      {
-            printf("\nEnter Details of Process[%d]\n", i + 1);
- 
-            printf("Arrival Time:\t");
- 
-            scanf("%d", &arrival_time[i]);
- 
-            printf("Burst Time:\t");
- 
-            scanf("%d", &burst_time[i]);
- 
-            temp[i] = burst_time[i];
-      }
- 
-      printf("\nEnter Time Quantum:\t");
-      scanf("%d", &time_quantum);
-      printf("\nProcess IDttBurst Timet Turnaround Timet Waiting Timen");
-      for(total = 0, i = 0; x != 0;)
-      {
-            if(temp[i] <= time_quantum && temp[i] > 0)
-            {
-                  total = total + temp[i];
-                  temp[i] = 0;
-                  counter = 1;
-            }
-            else if(temp[i] > 0)
-            {
-                  temp[i] = temp[i] - time_quantum;
-                  total = total + time_quantum;
-            }
-            if(temp[i] == 0 && counter == 1)
-            {
-                  x--;
-                  printf("\nProcess[%d]\t\t%d\t\t%d\t\t\t%d", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
-                  wait_time = wait_time + total - arrival_time[i] - burst_time[i];
-                  turnaround_time = turnaround_time + total - arrival_time[i];
-                  counter = 0;
-            }
-            if(i == limit - 1)
-            {
-                  i = 0;
-            }
-            else if(arrival_time[i + 1] <= total)
-            {
-                  i++;
-            }
-            else
-            {
-                  i = 0;
-            }
-      }
- 
-      average_wait_time = wait_time * 1.0 / limit;
-      average_turnaround_time = turnaround_time * 1.0 / limit;
-      printf("\n\nAverage Waiting Time:\t%f", average_wait_time);
-      printf("\nAvg Turnaround Time:\t%f\n", average_turnaround_time);
+      int n;
+      printf("\n\n Enter the number of queens:");
+      scanf("%d", &n);
+      queen(1, n);
       return 0;
 }
