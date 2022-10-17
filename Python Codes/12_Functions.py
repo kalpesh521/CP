c0dc398fa8b6aab25a43775a933fbc4d7c25b2d3
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-10-17 20:05:12 +0530

    Function

--- Python Codes/12_Functions.py
+++ Python Codes/12_Functions.py
@@ -1,73 +1,73 @@
 
-# def add_sub(x,y):
-#     c=x+y
-#     d=x-y
-#     return c,d
-# result1,result2 =add_sub(5,2)
-# print(result1,result2)
-# print(add_sub(4,2))
+def add_sub(x,y):
+    c=x+y
+    d=x-y
+    return c,d
+result1,result2 =add_sub(5,2)
+print(result1,result2)
+print(add_sub(4,2))
 
-# #Keyword Argument
-# def stud (name,age):
-#     print(age)
-#     print(name)
-# stud(age=21,name="kp")
+#Keyword Argument
+def stud (name,age):
+    print(age)
+    print(name)
+stud(age=21,name="kp")
 
-# #Default Argument
-# def stud (name,age=18):
-#     print(age)
-#     print(name)
-# stud("kalpesh")
-# #Variable Length Argument
-# def sum (a,*b):
-#     print(a)
-#     print(b)
-#     c=a
-#     for i in b:
-#         c+=i
-#     print(c)
+#Default Argument
+def stud (name,age=18):
+    print(age)
+    print(name)
+stud("kalpesh")
+#Variable Length Argument
+def sum (a,*b):
+    print(a)
+    print(b)
+    c=a
+    for i in b:
+        c+=i
+    print(c)
 
-# sum(2,3,6,8)
+sum(2,3,6,8)
 
-# #keyword Variable Length Argument
+#keyword Variable Length Argument
 
-# def key(name,**data):
-#     print(name)
-#     print(data)
-#     for i,j in data.items():
-#         print(i,j)
+def key(name,**data):
+    print(name)
+    print(data)
+    for i,j in data.items():
+        print(i,j)
         
-# key('Ram',age=23,Loc="Pune",Mob=3232324121)
+key('Ram',age=23,Loc="Pune",Mob=3232324121)
 
-# #Global and Local Variable function
+#Global and Local Variable function
 
-# a =5  
-# def glob():
-#     global a
-#     a =3
-#     print(a)
-#     globals() ['a']=12
+a =5  
+def glob():
+    global a
+    a =3
+    print(a)
+    globals() ['a']=12
 
-# glob()
-# print(a)
+glob()
+print(a)
 
-# #%%
-# #Count even /odd number in list 
-# lst =[3,4,12,67,32,5,6,2,4,11]
-# def count(lst):
-#     even =0
-#     odd =0
-#     for i in lst:
-#         if i%2==0:
-#             even+=1
-#         else:
-#             odd+=1
-#     return even,odd
-# even,odd =count(lst)
-# print("Even : {} and Odd : {}".format(even,odd))
+#%%
+#Count even /odd number in list 
+lst =[3,4,12,67,32,5,6,2,4,11]
+def count(lst):
+    even =0
+    odd =0
+    for i in lst:
+        if i%2==0:
+            even+=1
+        else:
+            odd+=1
+    return even,odd
+even,odd =count(lst)
+print("Even : {} and Odd : {}".format(even,odd))
 
-# #%%
-
+#%%
+# Fibonacci series Program
 # n =int(input("Enter number : "))
 # def fib(n):
     
@@ -85,16 +85,16 @@
 #          b=c
 #          print(c)
 # fib(n)
-# #%% 
-# #Factorial 
-# def fact(n):
-#     a=1
-#     for i in range(1,n+1):
-#         a =a*i
-#     return a
-# x=5
-# result=fact(x)
-# print(result)
+#%% 
+#Factorial 
+def fact(n):
+    a=1
+    for i in range(1,n+1):
+        a =a*i
+    return a
+x=5
+result=fact(x)
+print(result)
 #%%
 #Pallindrome or not
 def pallindrome(st):
