fa5c5d7083492345cf727b20340cba3e89814e70
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-10-21 21:40:59 +0530

    Prime

--- Python Codes/Python Problem Set/3_Find_Pairs.py
+++ Python Codes/Python Problem Set/3_Find_Pairs.py
@@ -2,7 +2,8 @@
 l=[1,2,3,5,7,11]
 def find_pairs(l,x):
     pairs=[]
-    for (i,el1) in enumerate(l):
+    for (i,el1) in enumerate(l): #enumerate adds a counter to an iterable 
+                                 #return in the form of enumerating object
         for (j,el2) in enumerate(l[i+1:]):
             if el1+el2 ==x:
                 pairs.append((el1,el2))
--- Python Codes/Python Problem Set/6_Intersection.py
+++ Python Codes/Python Problem Set/6_Intersection.py
@@ -1,5 +1,7 @@
 lst=[4,3,21,2]
 print(sorted(lst,key=lambda x:0 if x==21 else x))
+
+
 from math import pi 
 r=int(input("Enter radius \n"))
 print("The area of circle having radius " +str(r)+ " is " + str(pi*r**2))
