384e0d0acc0f4ab2c2c0797335ab43bf600ff5af
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-11-18 21:13:10 +0530

    Algorithm

--- C++ Codes/tempCodeRunnerFile.cpp
+++ C++ Codes/tempCodeRunnerFile.cpp
@@ -1,15 +1,1 @@
-int delete (struct Array *arr, int index)
-// {
-//     int x = 0;
-//     if (index >= 0 && arr->length > index)
-//     {
-//         x = arr->A[index];
-//         for (int i = index; i < arr->length - 1; i++)
-//         {
-//             arr->A[i] = arr->A[i + 1];
-//         }
-//         arr->length--;
-//         return x;
-//     }
-//     return 0;
-// }
+  
--- C++ Codes/trial.cpp
+++ C++ Codes/trial.cpp
@@ -1,59 +1,49 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-void printfunction(vector<vector<int>> &board)
+
+void printsolution(vector<vector<int>> &board)
 {
     for (int i = 0; i < board.size(); i++)
     {
         for (int j = 0; j < board.size(); j++)
         {
             cout << board[i][j] << " ";
-            
         }
         cout << endl;
     }
     cout << endl;
 }
 
-bool isSafe(vector<vector<int>> &board, int row, int col)
+bool issafe(vector<vector<int>> &board, int row, int col)
 {
     int i, j;
-    for (i = 0; i < col; i++)
+    for (int i = 0; i < col; i++)
     {
         if (board[row][i])
-        {
             return false;
-        }
     }
-    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
     {
         if (board[i][j])
             return false;
     }
-    for (i = row, j = col; i < board.size() && j >= 0; i++, j--)
+    for (int i = row, j = col; i < board.size() && j >= 0; i++, j--)
     {
         if (board[i][j])
             return false;
     }
     return true;
 }
-
-bool solvequeen(vector<vector<int>> &board, int col)
+bool nqueen(vector<vector<int>> &board, int col)
 {
-
     if (col >= board.size())
-    {
         return true;
-    }
-
     for (int i = 0; i < board.size(); i++)
     {
-        if (isSafe(board, i, col))
+        if (issafe(board, i, col))
         {
             board[i][col] = 1;
-
-            if (solvequeen(board, col + 1))
+            if (nqueen(board, col + 1))
             {
                 return true;
             }
@@ -63,37 +53,22 @@ bool solvequeen(vector<vector<int>> &board, int col)
     return false;
 }
 
-int main()
+int main(int argc, char const *argv[])
+
 {
-    // cout << "enter number of queen" << endl;
-    // int n;
-    // cin >> n;
-    // vector<vector<int>> board(n, vector<int>(n, 0));
-    // if (solvequeen(board, 0) == false)
-    // {
-    //     cout << "Solution not exist";
-    //     return 0;
-    // }
-    // else
-    // {
-    //     cout << "The soution of " << n << "queen";
-    //     printfunction(board);
-    // }
-    // return 0;
     int n;
     cout << "Enter the number of queens: ";
     cin >> n;
     vector<vector<int>> board(n, vector<int>(n, 0));
-    if (solvequeen(board, 0) == false)
+    if (nqueen(board, 0) == false)
     {
         cout << "Solution does not exist";
-        return 0;
+    
     }
     else
     {
         cout << "Solution for " << n << " queen is : \n";
-        printfunction(board);
+        printsolution(board);
     }
-
     return 0;
 }
