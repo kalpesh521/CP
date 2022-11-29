aa20de08eda0f31b3d28fec751db742843dd1450
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-11-29 16:47:21 +0530

    java

--- Java/Java Codes/src/Basic.java
+++ Java/Java Codes/src/Basic.java
@@ -16,7 +16,7 @@ public class Basic {
         int n1=10;
         int n2=20;
         System.out.println((n1>n2)? n1:n2);
-        out.println("Kalpesh");
+        out.println("Africa");
         out.println("India");
 
 
