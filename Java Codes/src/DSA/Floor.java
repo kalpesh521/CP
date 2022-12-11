ab1c844b1815d105defcb3e5913488b1b5a23121
“Kalpesh <kalpeshpawar7875@gmail.com>
2022-12-11 22:48:26 +0530

    SmallestInteger

--- Java Codes/src/DSA/Floor.java
+++ Java Codes/src/DSA/Floor.java
@@ -23,3 +23,4 @@ public class Floor {
         return  end;
     }
 }
+ 
---
+++ SmallestInteger.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+//Question No. : 744
+public class SmallestInteger {
+
+    public static void main(String[] args) {
+        char[] letters = { 'c', 'f', 'j' };
+        char target = 'd';
+        char ans = nextGreatestLetter(letters, target);
+        System.out.println(ans);
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+
+            }
+        }
+        return letters[start % letters.length];
+
+    }
+}
