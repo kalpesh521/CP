a0ad9719838d61c2b172f705dbe3d21a81b42813
“Kalpesh <kalpeshpawar7875@gmail.com>
2022-12-24 18:19:21 +0530

    Singleton Class

---
+++ Java Codes/src/OOPs/OOPs/StaticExamples/NewClass.java
@@ -0,0 +1,9 @@
+package OOPs.OOPs.StaticExamples;
+
+public class NewClass {
+    public static void main(String[] args) {
+         Singleton obj1= Singleton.getInstance();
+         Singleton obj2= Singleton.getInstance();
+         Singleton obj3= Singleton.getInstance();
+    }
+}
