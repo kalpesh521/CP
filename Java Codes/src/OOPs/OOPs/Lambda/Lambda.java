76df2a9e9a51b5cdd40137123bacaf6b05e1743a
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-01-08 18:50:37 +0530

    Lambda Function

--- Java Codes/src/OOPs/OOPs/Lambda/Lambda.java
+++ Java Codes/src/OOPs/OOPs/Lambda/Lambda.java
@@ -2,7 +2,6 @@ package OOPs.OOPs.Lambda;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
-//Lambda function
 public class Lambda {
 
     public static void main(String[] args) {
