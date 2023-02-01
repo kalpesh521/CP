e2979c642059d83e45b8d2f3217b84cb34f8cb57
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-02-01 16:02:49 +0530

    BuyAndSell

--- Java Codes/src/Leetcode/Array/BuyAndSell121.java
+++ Java Codes/src/Leetcode/Array/BuyAndSell121.java
@@ -1,5 +1,12 @@
 package Leetcode.Array;
 
+// 121. Best Time to Buy and Sell Stock
+/*
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+*/
 public class BuyAndSell121 {
     public static int getProfit(int[] arr) {
         int min = Integer.MAX_VALUE;
@@ -17,7 +24,7 @@ public class BuyAndSell121 {
     }
 
     public static void main(String[] args) {
-        int [] arr = {7,1,5,3,6,4};
+        int[] arr = {7, 1, 5, 3, 6, 4};
         System.out.println(getProfit(arr));
-     }
+    }
 }
