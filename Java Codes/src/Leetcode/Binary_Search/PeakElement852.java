9a98cb9318255cad0b7f41edece9eaba07fdeb1f
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-02-23 16:19:05 +0530

    Peak element

--- Java Codes/src/Leetcode/Binary_Search/PeakElement852.java
+++ Java Codes/src/Leetcode/Binary_Search/PeakElement852.java
@@ -2,18 +2,25 @@ package Leetcode.Binary_Search;
 // Leetcode 852
 public class PeakElement852 {
 
-    public int peakIndexInMountainArray(int[] arr) {
+    public static int peakIndexInMountainArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
         while (start < end) {
             int mid = start + (end - start) / 2;
             if (arr[mid] > arr[mid + 1]) {
-                end = mid;
+                end = mid; //{2,3,4,5,7}
+                           // s       e
             } else {
-                start = mid + 1;
+                start = mid + 1;//{3,2,1}
+                                // s   e
             }
         }
-        return start;
+        return start;//return peak element index
+    }
+
+    public static void main(String[] args) {
+        int [] arr ={ 2,3,4,5,7,3,2,1};
+        System.out.println(peakIndexInMountainArray(arr));
     }
 }
 
