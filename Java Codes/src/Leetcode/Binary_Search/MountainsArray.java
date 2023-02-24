2ca29ce47e333056a669476cf887680f1473eab5
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-02-24 22:21:46 +0530

    Rotated Search Array

--- Java Codes/src/Leetcode/Binary_Search/MountainsArray.java
+++ Java Codes/src/Leetcode/Binary_Search/MountainsArray.java
@@ -1,22 +1,26 @@
 package Leetcode.Binary_Search;
 
+//Approach
+//1. Find Peak element
+//2. Search in first half left to the peak element (ASC)
+//3. Search in Second half right to the peak element using order Agnostic Search (DESC)
 public class MountainsArray {
     public static void main(String[] args) {
-        int [] arr ={1,4,5,6,7,3,2,};
+        int[] arr = {1, 4, 5, 6, 7, 3, 2,};
         int target = 5;
-        System.out.println(search(arr,target));
+        System.out.println(search(arr, target));
     }
 
     static int search(int[] arr, int target) {
         int peak = peakIndexInMountainArray(arr);
-        int firstTry =  OrderBS(arr, target, 0, peak);
+        int firstTry = OrderBS(arr, target, 0, peak);
         if (firstTry != -1) {
             return firstTry;
         }
         // try to search in second half
-        return OrderBS(arr, target, peak+1, arr.length - 1);
+        return OrderBS(arr, target, peak + 1, arr.length - 1);
     }
-
+    //Find Peak Element
     public static int peakIndexInMountainArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
@@ -33,10 +37,11 @@ public class MountainsArray {
         return start;//return peak element index
     }
 
-    static int OrderBS(int[] arr, int target,int start ,int end) {
+    //order Agnostic binary search for asc/desc search
+    static int OrderBS(int[] arr, int target, int start, int end) {
 
 
-        boolean isAsc = arr[start] < arr[end];//if As
+        boolean isAsc = arr[start] < arr[end];
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
--- Java Codes/src/Leetcode/Binary_Search/RotatingArraySearch33.java
+++ Java Codes/src/Leetcode/Binary_Search/RotatingArraySearch33.java
@@ -2,7 +2,7 @@ package Leetcode.Binary_Search;
 
 public class RotatingArraySearch33 {
 
-    public int search(int[] nums, int target) {
+    public static  int search(int[] nums, int target) {
 
         int pivot = pivot(nums);
         if (pivot == -1) {
@@ -12,13 +12,13 @@ public class RotatingArraySearch33 {
             return pivot;
         }
         if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
+            return binarySearch(nums, target, 0, pivot - 1);//first half left to the pivot
         } else {
-            return binarySearch(nums, target, pivot + 1, nums.length - 1);
+            return binarySearch(nums, target, pivot + 1, nums.length - 1);//Second half right to the pivot
         }
     }
 
-    int binarySearch(int[] nums, int target, int start, int end) {
+    static int binarySearch(int[] nums, int target, int start, int end) {
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
@@ -35,24 +35,30 @@ public class RotatingArraySearch33 {
     }
 
 
-    int pivot(int[] nums) {
+     static  int  pivot(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (mid < end && nums[mid] > nums[mid + 1]) {
-                return mid;
+                return mid;// mid is pivot
             }
             if (mid > start && nums[mid] < nums[mid - 1]) {
                 return mid - 1;
             }
             if (nums[mid] <= nums[start]) {
-                end = mid - 1;
+                end = mid - 1;//Ignore second half right to the mid
             } else {
-                start = mid + 1;
+                start = mid + 1;//Ignore first half left to the mid
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr= { 2,3,4,5,7,3,2,1};
+        int target = 5;
+        System.out.println(search(arr,target));
     }
 }
 
