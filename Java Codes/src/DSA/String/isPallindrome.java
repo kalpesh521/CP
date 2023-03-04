3225bb05bdb1714b89e1b48bbdca38163e369e89
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-03-04 22:16:29 +0530

    Pallindrome

--- Java Codes/src/DSA/String/isPallindrome.java
+++ Java Codes/src/DSA/String/isPallindrome.java
@@ -1,5 +1,5 @@
 package DSA.String;
-// Program to check whether String is pallindrome or not
+// WAP Program to check whether String is pallindrome or not
 public class isPallindrome {
     public static void main(String[] args) {
     String str = "Madamn";
