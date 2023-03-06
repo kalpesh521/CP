21844928819b34a8142261fdd8084140c465d1fc
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-03-06 23:00:53 +0530

    kth Missing Number

---
+++ Java Codes/src/Leetcode/Array/kthMissingPositiveNumber1539.java
@@ -0,0 +1,16 @@
+package Leetcode.Array;
+
+public class kthMissingPositiveNumber1539 {
+    public static int findKthPositive(int[] arr, int k) {
+        for (int i : arr) {
+            if (i <= k) k++;
+            else break;
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int [] arr= {1,2,3,5,7,8,10};
+        System.out.println(findKthPositive(arr,3));
+    }
+}
