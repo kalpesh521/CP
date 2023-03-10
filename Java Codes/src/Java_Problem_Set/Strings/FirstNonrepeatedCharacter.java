660941e7136e56e969596774c26a79e701c3497a
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-03-10 22:25:33 +0530

    occurences

---
+++ Java Codes/src/Java_Problem_Set/Strings/FirstNonrepeatedCharacter.java
@@ -0,0 +1,30 @@
+package Java_Problem_Set.Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Java Program To Find First Non Repeated Character
+
+public class FirstNonrepeatedCharacter {
+    public static void main(String[] args) {
+        String str = "ABCACDBEAFB";
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        System.out.println(map);
+
+        for (Map.Entry<Character, Integer> entryset : map.entrySet()) {
+            if (entryset.getValue() == 1) {
+                System.out.println(entryset.getKey());
+                break;
+            }
+        }
+    }
+}
