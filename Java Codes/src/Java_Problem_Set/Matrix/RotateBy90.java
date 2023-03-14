af22ec747cd6336adbb7c49b7a110bab7b14bebc
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-03-14 18:44:33 +0530

    Rotated Array 2D

---
+++ Java Codes/src/Java_Problem_Set/Matrix/RotateBy90.java
@@ -0,0 +1,54 @@
+package Java_Problem_Set.Matrix;
+
+import java.util.Scanner;
+
+public class RotateBy90 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows of first matrix");
+        int n = sc.nextInt();
+        System.out.println("Enter number of columns of first matrix");
+        int m = sc.nextInt();
+        System.out.println("Enter Row and Column of first Matrix");
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            int li =0;
+            int ri =arr[i].length -1;
+            while(li <ri){
+                int temp = arr[i][li];
+                arr[i][li] = arr[i][ri];
+                arr[i][ri] = temp;
+
+                li++;
+                ri--;
+            }
+        }
+
+        System.out.println("Rotated Array : ");
+        display(arr);
+    }
+
+    static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
