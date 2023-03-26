53c0f162db366be9cecb86edf43925adeb86b221
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-03-26 23:12:27 +0530

    reverse Linkedlist

---
+++ Java Codes/src/Leetcode/LinkedList/ReverseList206.java
@@ -0,0 +1,27 @@
+package Leetcode.LinkedList;
+
+public class ReverseList206 {
+
+    public void reverse(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        head = prev;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
