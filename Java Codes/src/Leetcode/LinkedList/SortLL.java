d0fd9091bbdbe8a9543b949e768e5d8f15ed0f05
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-03-26 22:21:30 +0530

    Sort Linkedlist

---
+++ Java Codes/src/Leetcode/LinkedList/SortLL.java
@@ -0,0 +1,45 @@
+package Leetcode.LinkedList;
+
+// Used Merge Sort to sort list , We can use recursive bubble sort also.
+public class SortLL {
+
+    public ListNode sortlist(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid =getMid(head);
+        ListNode left =  sortlist(head);
+        ListNode right = sortlist(mid);
+        return  merge(left,right);
+    }
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return head.next;
+    }
+
+    ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+    }
+}
