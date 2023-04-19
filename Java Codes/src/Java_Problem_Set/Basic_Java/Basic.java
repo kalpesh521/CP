6796d54e1c8222e016e357671e48d526844b45d9
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-04-19 21:44:22 +0530

    star

--- Java Codes/src/Java_Problem_Set/Basic_Java/Basic.java
+++ Java Codes/src/Java_Problem_Set/Basic_Java/Basic.java
@@ -2,7 +2,7 @@ package Java_Problem_Set.Basic_Java;
 //star
 import java.math.BigInteger;
 import java.util.Scanner;
-//
+
 public class Basic {
 
     // Command Line Argument in Java is the information that is passed to the program
