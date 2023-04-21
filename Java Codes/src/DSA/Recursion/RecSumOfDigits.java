f7c57d592e607a7f72493c45360dbb800a5f651c
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-04-21 10:04:49 +0530

    Recursion

--- Java Codes/src/DSA/Recursion/RecSumOfDigits.java
+++ Java Codes/src/DSA/Recursion/RecSumOfDigits.java
@@ -2,15 +2,27 @@ package DSA.Recursion;
 
 public class RecSumOfDigits {
     public static void main(String[] args) {
+        System.out.println("Sum of digits is :");
         int res = sumOfdigits(1234);
         System.out.println(res);
-    }
+        System.out.println("Product of digits is :");
+        int res2=ProductOfdigits(1234);
+        System.out.println(res2);
+     }
 
     static int sumOfdigits(int n) {
         if (n == 0) {
             return 0;
         }                  //123            4
-                           //12           34
+        //12           34
         return sumOfdigits(n / 10) + (n % 10);
+    }
+
+    static int ProductOfdigits(int n) {
+        if (n == 0) {
+            return 0;
+        }                  //123            4
+        //12           34
+        return sumOfdigits(n / 10) * (n % 10);
     }
 }
