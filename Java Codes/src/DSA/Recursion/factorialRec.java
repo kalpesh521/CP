b788dc5c4eb22866356e11b987f2a74f4e00defe
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-04-21 09:47:31 +0530

    BS

---
+++ Java Codes/src/DSA/Recursion/factorialRec.java
@@ -0,0 +1,12 @@
+package DSA.Recursion;
+
+public class factorialRec {
+    public static void main(String[] args) {
+       int res= factorial(5);
+        System.out.println(res);
+    }
+    static  int factorial(int n){
+        if(n==1) return  1;
+        return n*factorial(n-1);
+    }
+}
