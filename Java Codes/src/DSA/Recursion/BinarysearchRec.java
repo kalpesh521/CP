9799ca5ca69fd3ac1278a40ff756ad69b08e5a4a
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-04-21 09:23:48 +0530

    BS

---
+++ Java Codes/src/DSA/Recursion/BinarysearchRec.java
@@ -0,0 +1,27 @@
+package DSA.Recursion;
+
+public class BinarysearchRec {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 88, 99};
+        int target = 6;
+        System.out.println(search(arr, target, 0, arr.length - 1));
+    }
+
+    static int search(int[] arr, int target, int s, int e) {
+
+        if (s > e) {
+            return -1;
+        }
+
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (target < mid) {
+            search(arr, target, s, mid - 1);
+        }
+        return search(arr, target, mid + 1, e);
+    }
+
+}
---
+++ Java Codes/src/DSA/Recursion/fibnacci.java
@@ -0,0 +1,15 @@
+package DSA.Recursion;
+
+public class fibnacci {
+    public static void main(String[] args) {
+
+        System.out.println(fib(4));
+    }
+
+    static  int fib(int n){
+        if(n<2){
+            return  n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}
