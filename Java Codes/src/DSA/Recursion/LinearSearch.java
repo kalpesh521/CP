fdf8cc8342ae500d02162bd7feb50837b9199dca
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-05-05 18:56:15 +0530

    Rec

--- Java Codes/src/DSA/Recursion/LinearSearch.java
+++ Java Codes/src/DSA/Recursion/LinearSearch.java
@@ -24,6 +24,8 @@ public class LinearSearch {
         searchDIn(arr, target, i + 1);
     }
 
+    // Passing arraylist in parameter and return it
+
     static ArrayList<Integer> searchAll(int arr[], int target, int i, ArrayList<Integer> list2) {
         if (i == arr.length) {
             return list2;
@@ -36,16 +38,35 @@ public class LinearSearch {
         return searchAll(arr, target, i + 1, list2);
     }
 
+    // Find Index of elements without passing arraylist in paramter and return it .
+    static ArrayList<Integer> searchAll2(int arr[], int target, int i) {
+        ArrayList<Integer> list2 = new ArrayList<>();
+        if (i == arr.length) {
+            return list2;
+        }
+        if (arr[i] == target) {
+            list2.add(i);
+
+        }
+        ArrayList<Integer> addAllBellowCalls = searchAll2(arr, target, i + 1);
+        list2.addAll(addAllBellowCalls);
+        return list2;
+    }
+
     public static void main(String[] args) {
         int arr[] = {2, 3, 4, 4, 5, 1};
-        int arr2[] = { 13, 14, 14, 5, 1};
+        int arr2[] = {13, 14, 14, 5, 1};
+        int arr3[] = {11,11,12, 13, 14, 14, 5, 1};
         System.out.println(search(arr, 7, 0));
 
         searchDIn(arr, 4, 0);
         System.out.println(list);
 
-         ArrayList<Integer> ans =searchAll(arr2, 14, 0,new ArrayList<>());
+        ArrayList<Integer> ans = searchAll(arr2, 14, 0, new ArrayList<>());
         System.out.println(ans);
+
+        ArrayList<Integer> ans2 = searchAll2(arr3 , 14, 0);
+        System.out.println(ans2);
 
     }
 }
