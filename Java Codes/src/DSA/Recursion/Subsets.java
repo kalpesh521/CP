20016e1ebb069ad68dd1401ea95b9c333c3784c8
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-05-06 18:59:03 +0530

    Rec String

---
+++ Java Codes/src/DSA/Recursion/Subsets.java
@@ -0,0 +1,61 @@
+package DSA.Recursion;
+
+import java.util.ArrayList;
+
+public class Subsets {
+    public static void main(String[] args) {
+        subseq("", "abc");
+        System.out.println("================ Ascii Values =================");
+        subseqAscii("", "abc");
+
+        ArrayList<String >res = subset("","abcd");
+        System.out.println(res);
+    }
+
+    /*
+     * p / up
+     * " "  / abc
+     * a / bc
+     * ab /c
+     * abc / " "
+     * */
+    static void subseq(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subseq(p + ch, up.substring(1));
+        subseq(p, up.substring(1));//Call all subsets
+    }
+
+    static void subseqAscii(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subseqAscii(p + ch, up.substring(1));
+        subseqAscii(p, up.substring(1));//Call all subsets
+        subseqAscii(p+ (ch +0), up.substring(1));//Call all subsets
+    }
+
+    //Add subsets in Arraylist
+
+    static ArrayList<String> subset(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subset(p + ch, up.substring(1));
+        ArrayList<String> right = subset(p, up.substring(1));
+        left.addAll(right);
+        return left;
+    }
+
+}
