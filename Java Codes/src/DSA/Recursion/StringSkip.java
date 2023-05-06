85ebedf9dc69e8c671306dcc71952ecf44fcc3a2
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-05-06 17:07:17 +0530

    Rec String

---
+++ Java Codes/src/DSA/Recursion/StringSkip.java
@@ -0,0 +1,53 @@
+package DSA.Recursion;
+
+public class StringSkip {
+
+    public static void main(String[] args) {
+        skip("", "baabcdad");
+        System.out.println(skip2("baab"));
+    }
+
+    /*
+     *  p  / up
+     *  " " / "baabc"
+     *  "b" / "aabc"
+     *  "b" / "abc" skip a
+     *    ....
+     *
+     * */
+
+    // Skip Character  by recursive call
+    static void skip(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char c = up.charAt(0);
+
+        if (c == 'a') {
+            skip(p, up.substring(1));
+        } else {
+            skip(p + c, up.substring(1));
+
+        }
+
+    }
+
+    // Return String
+    static String skip2(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char c = up.charAt(0);
+
+        if (c == 'a') {
+            return skip2(up.substring(1));
+        } else {
+            return c +  skip2(up.substring(1));
+
+        }
+
+    }
+}
