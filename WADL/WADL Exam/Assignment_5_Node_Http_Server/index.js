592982a7bff696aacd89375fa0e5a40f134dbc53
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-05-16 08:24:51 +0530

    react

--- WADL/WADL Exam/Assignment_5_Node_Http_Server/index.js
+++ WADL/WADL Exam/Assignment_5_Node_Http_Server/index.js
@@ -1,13 +1,18 @@
 
 //  https://www.guru99.com/node-js-create-server-get-data.html
-var http=require('http')
-var server=http.createServer((function(request,response)
+
+ 
+
+var http=require('http') // Import Node.js core module
+
+var server=http.createServer((function(request,response) //	Create a server object
 {
-	response.writeHead(200,
-	{"Content-Type" : "text/plain"});
-	response.end("Hello from kalpesh\n");
+	response.writeHead(200, 
+	{"Content-Type" : "text/plain"});// Send the HTTP header HTTP Status: 200 : OK Content Type: text/plain
+	response.end("Hello from kalpesh\n");// Send the response body "Hello World"
 }));
-server.listen(8000,()=>{
-    console.log("Server is running on port 8000");
+server.listen(8000,()=>{ // Bind server to a port
+    console.log("Server is running on port 8000"); // Run the server
 });
+
 
