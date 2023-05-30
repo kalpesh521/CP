96f46f5b7c00192e459c11cba76971d3743744be
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-05-30 07:26:22 +0530

    comit

--- WADL/WADL Exam/Authentication/index.js
+++ WADL/WADL Exam/Authentication/index.js
@@ -82,6 +82,6 @@ app.get('/logout', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
