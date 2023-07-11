ed8f812dda3f99e52bf7349ea60afa4c032fc12d
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-07-11 22:48:34 +0530

    Commit

--- Java Codes/src/Java_Problem_Set/Java_Course/hello.java
+++ Java Codes/src/Java_Problem_Set/Java_Course/hello.java
@@ -2,7 +2,7 @@ package Java_Problem_Set.Java_Course;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Kalpesh");
+        System.out.println("Hello Kalpesh");
         System.out.println(args[0]);
     }
 }
