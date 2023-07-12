1c0ca6d2145d7c534d815c31e4082445605a3913
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-07-12 22:22:23 +0530

    Commit

---
+++ Java Codes/src/Java_Problem_Set/Java_Course/Sting.java
@@ -0,0 +1,15 @@
+package Java_Problem_Set.Java_Course;
+
+public class Sting {
+    public static void main(String[] args) {
+        char c[] ={'K','A','L','P','E','S','H'};
+        byte b[]={65,66,67,68};
+
+        String str1= new String(c,2,3); //char
+        String str2= new String(b,1,2); //byte
+        String str3= new String(str2);  //string
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
