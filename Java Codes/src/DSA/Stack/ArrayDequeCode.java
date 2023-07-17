22a343687de81299dcea5a96a6fec9472be54038
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-07-17 22:47:18 +0530

    Stack

---
+++ Java Codes/src/DSA/Stack/ArrayDequeCode.java
@@ -0,0 +1,16 @@
+package DSA.Stack;
+
+import java.util.*;
+
+public class ArrayDequeCode {
+    public static void main(String[] args) {
+        Deque<Integer> dq= new ArrayDeque<Integer>();
+        dq.push(5);
+        dq.push(2);
+        dq.push(1);
+        dq.push(6);
+        System.out.println(dq.peek());
+        System.out.println(dq.pop());
+        System.out.println(dq);
+    }
+}
