7f6a4be64c093ad1e338ade7559fa55a3a0387cc
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-08-14 22:55:46 +0530

    Binary

--- Java Codes/src/DSA/Recursion/BinarysearchRec.java
+++ Java Codes/src/DSA/Recursion/BinarysearchRec.java
@@ -1,12 +1,12 @@
 package DSA.Recursion;
 
+
 public class BinarysearchRec {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         int[] arr = {2, 4, 6, 88, 99};
         int target = 6;
         System.out.println(search(arr, target, 0, arr.length - 1));
     }
-
     static int search(int[] arr, int target, int s, int e) {
 
         if (s > e) {
@@ -23,5 +23,4 @@ public class BinarysearchRec {
         }
         return search(arr, target, mid + 1, e);
     }
-
 }
