bc1c26aab28a90f97167606b9034f4c63183b7ae
“Kalpesh <kalpeshpawar7875@gmail.com>
2023-08-29 23:16:52 +0530

    String

---
+++ Java Codes/src/DSA/String/ReverseStering.java
@@ -0,0 +1,15 @@
+package DSA.String;
+
+public class ReverseStering {
+    public static void main(String[] args) {
+        String str = "HELLO";
+
+        StringBuilder str1 = new StringBuilder(str);
+        str1.reverse();
+
+        str=str1.toString();
+
+        // print reversed String
+        System.out.println("The reversed string is- "+str);
+     }
+}
